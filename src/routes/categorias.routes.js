4af531c0d0cbb4d2c735cf72a43b0ba8c4fcb95d
Ivan250505 <ivvh2505@gmail.com>
2025-04-16 21:52:02 -0500

    Actualizar Registros - Método HTTP PUT

--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -8,7 +8,8 @@ router.get("/",categoriaController.getCategorias); /*read*/
 router.post("/",categoriaController.postCategorias);/*create */
 /*ruta que recibe un parametro */
 router.get("/:id",categoriaController.getCategory);/*create */
-
+/*Ruta que recibe como parametro el cuerpo del id a actualizar */
+router.put("/:id",categoriaController.updateCategorias);/*create */
 
 /*hacemos disponible el router en otda la app */
 export default router;
